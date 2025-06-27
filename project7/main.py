44cf270d26ef61718a0f2683ddf905f7fc6011a5
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-06-27 16:59:55 +0530

    finish implementation

--- project7/main.py
+++ project7/main.py
@@ -2,12 +2,19 @@ from Parser import Parser
 from Codewriter import CodeWriter
 def main():
     # Initialize the parser with the input file
-    parser = Parser("input.vm")
+    import sys
+
+    if len(sys.argv) != 3:
+        print("Usage: python myscript.py input.vm output.asm")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    parser = Parser(input_file)
     parser.parse()  # Read and parse the VM commands
 
     # Initialize the code writer with the output file
     code_writer = CodeWriter()
-    code_writer.setFileName("output.asm")
+    code_writer.setFileName(output_file)
 
     # Process each command in the parsed data
     while parser.hasMoreCommands():
@@ -24,3 +31,6 @@ def main():
 
     # Close the output file
     code_writer.file.close()
+
+if __name__ == "__main__":
+    main()
